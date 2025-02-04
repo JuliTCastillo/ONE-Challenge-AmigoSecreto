af4280bd23bba8fd61506841e908b2146c75d201
Matias Garcete <julietatcastillo04@gmail.com>
2025-02-04 13:33:42 -0300

    se agrego nuevas funcionalidades

--- app.js
+++ app.js
@@ -3,16 +3,51 @@ let listaAmigos = []
 
 function agregarAmigo(){
     //obtenemos el valor del input-text
-    let nombreAmigo = document.getElementById("amigo")
-    if (nombreAmigo.value){
-        elementoHTML("msg", "")
-        listaAmigos.push(nombreAmigo)
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo){ //Verificamos que se haya ingresado algo
+        elementoHTML("msg", ""); //limpiamos el parrafo de mensaje
+        if(!listaAmigos.includes(nombreAmigo)){ //verificamos que el elemento que se quiera ingrese no exista en la lista
+            elementoHTML("msg", ""); //vaciamos el parrafo
+            listaAmigos.push(nombreAmigo); //guardamos el elemento en la lista
+            document.getElementById("amigo").value = ""; //vaciamos el input
+            actualizarLista(listaAmigos); //lo mostramos en la web
+        }
+        else{
+            elementoHTML("msg", "❕Este amigo ya esta en la lista");
+        }
     }
     else{
-        elementoHTML("msg", "❕Ingrese un nombre por favor!")
+        elementoHTML("msg", "❕Ingrese un nombre por favor!");
+    }
+    console.log(listaAmigos);
+}
+
+function sortearAmigo(){
+    let cantidad = listaAmigos.length;
+    let resultado = document.getElementById("resultado")
+    if(cantidad > 0 ){
+        let posicion = Math.floor(Math.random() * cantidad)
+        resultado.innerHTML = `
+            <p>Su amigo secreto es...</p>
+            <p>${listaAmigos[posicion]}</p>
+        `
+    }
+    else{
+        resultado.style.color = "red"
+        resultado.innerHTML = "Su lista de amigos esta vacia"
     }
 }
 
+function actualizarLista(lista){
+    //Obtenemos el elemento
+    let elementoLista = document.getElementById("listaAmigos");
+    elementoLista.innerHTML = ""; //Vaciamos la etiqueta
+
+    lista.forEach(element => { //Recorremos la lista y guardamos los elementos en una variable
+        elementoLista.innerHTML += `<li>${element}</li>` //Mostramos el elemento en un item
+    });
+}
+
 function elementoHTML(id, mensaje){
-    document.getElementById(id).innerText = mensaje
+    document.getElementById(id).innerText = mensaje;
 }
